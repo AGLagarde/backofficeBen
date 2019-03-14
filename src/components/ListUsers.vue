99acf61c301f0d77745e2f47a497110160009e53
Anne <anne.lagarde@hetic.net>
2019-03-14 16:44:13 +0100

    placeholder json data

--- src/components/ListUsers.vue
+++ src/components/ListUsers.vue
@@ -4,10 +4,9 @@
     <table class="listUsers__table"> 
       <tr class="listUsers__table__head">
         <th>#</th>
-        <th>Prénom</th> 
-        <th>Nom</th>
+        <th>Name</th> 
+        <th>Username</th>
         <th>Mail</th>
-        <th>Like</th>
         <th>Actions</th>
       </tr>
       <tr class="listUsers__table__body" 
@@ -17,23 +16,19 @@
         v-bind:key="user.id"
         >
         <th class="listUsers__table__body-entries"><input class="input-firstname" type="text" disabled="true" v-bind:value="user.id"></th> 
-        <th class="listUsers__table__body-entries"><input class="input-firstname" type="text" disabled="true" v-bind:value="user.firstname" v-on:keyup.enter="editUser"></th> 
-        <th class="listUsers__table__body-entries"><input class="input-name" type="text" disabled="true" v-bind:value="user.lastname" v-on:keyup.enter="editUser"></th>
-        <th class="listUsers__table__body-entries"><input class="input-mail" type="text" disabled="true" v-bind:value="user.mail" v-on:keyup.enter="editUser"></th>
-        <th class="listUsers__table__body-entries"><input class="input-likes" type="text" disabled="true" v-bind:value="user.likes" v-on:keyup.enter="editUser"></th>
+        <th class="listUsers__table__body-entries"><input class="input-firstname" type="text" disabled="true" v-bind:value="user.name" v-on:keyup.enter="editUser"></th> 
+        <th class="listUsers__table__body-entries"><input class="input-name" type="text" disabled="true" v-bind:value="user.username" v-on:keyup.enter="editUser"></th>
+        <th class="listUsers__table__body-entries"><input class="input-mail" type="text" disabled="true" v-bind:value="user.email" v-on:keyup.enter="editUser"></th>
         <th class="listUsers__table__body-actions">
           <ul>
             <li v-on:click="editUser">{{ msgAction }}</li>
-            <li v-on:click="deleteConfirmationUser">Supprimer</li>
+            <li v-on:click="">Supprimer</li>
           </ul>
         </th>
       </tr>
     </table>
 
-    <Popin/>
-
     <!-- cf modele backoffice : https://vuejsexamples.com/basic-vuejs-data-table/ -->
-    <button v-on:click="getUser">Get Users</button>
     <button v-on:click="createUser">Create User</button>
   </div>
   
@@ -42,32 +37,35 @@
 <script>
 import {HTTP} from '../http-constant'
 import axios from 'axios'
-import Popin from './Popin.vue'
 
 
 export default {
   name: 'listUsers',
-  components: {
-    Popin
-  },
   data() {
     return {
-      // users : {
-      //   firstname:'',
-      //   lastname: '',
-      //   email: '',
-      //   password: ''
-      // },
-      users: [
-        { id: 33, firstname: 'Anna', name: 'Lagarde', mail: 'allie@gmail.com', likes: 10 }, 
-        { id: 44, firstname: 'Olga', name: 'Fritz', mail: 'money@gmail.com', likes: 22 },
-        { id: 55, firstname: 'Tosupress', name: 'Tosupress', mail: 'Tosupress@gmail.com', likes: 0 }
-      ], 
-      title: 'Colocataires', 
-      msgAction: 'Editer', 
-      deletedItem : false
+      title: 'JSON Placeholder',
+      msgAction: 'Editer',
+      users : {
+        name:'',
+        username: '',
+        email: ''
+      }
     }
   },
+   // lifecycle hooks to GET all users in API
+  created: function() {
+    axios({
+      method: 'get',
+      url: 'https://jsonplaceholder.typicode.com/users',
+    })
+      .then(response => {
+        this.users = response.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },
+
   methods: {
       editUser: function(event) {
         let domScope = event.target.parentNode.parentNode.parentNode;
@@ -83,6 +81,8 @@ export default {
           this.msgAction = 'Editer'
           inputs.forEach(input => input.setAttribute('disabled', 'true'))
         }
+
+        // envoyer PUT request
       },
 
       deleteUser: function(entry) {
@@ -96,54 +96,28 @@ export default {
         // console.log(this.users)
       }, 
 
-      
-
-      // HTTP.get(...).then(...).catch(...)
-        
-          // http://ulysse.idequanet.com/ben/web/api/user
-          // un user : repo, stars
-          // travailler l'authentification token  jwt ou oAuth2
-          // JSON: renvoie un objet clé:users
-
-      // search bar 
-      // https://codepen.io/AndrewThian/pen/QdeOVa
-
-      getUser: function() {
-        axios({
-          method: 'get',
-          url: 'http://ulysse.idequanet.com/ben/web/api/users',
-          headers: {
-            Authorization: "BEARER eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1NTI1NjQyNjYsImV4cCI6MTU1MjU3NTA2Niwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImVtYWlsIjoiam90ZXNzc2VAZ21haWwuY29tIn0.SO-brZ3PEHStRJo9vfllIsazTFjfbzr_IKr4RcXuFfeYaH_02Eu3k6csws6s7YcATpyWSfM7LIk7V6OjnperjadVdQTtHQBLBuKkj6v6kOn_BMiTARck-7JYLVFJCoBfv3a8VjiqkI3NGfrqzHdTv89tjs_ktX8mdcN5ljrC05gzjeBu8I2107Xg0UGLQVbqO0OyXfVHEROTv6Ws7pOPJBqD-JCwRkM0diK_2B94nGKaDBEOkWCbTSgtrnCok5KAvtVquVsEsvKv4aTUWPf_swlCkrz9FP92Dv1ilupS7KM7NYDcct8aEUMp-w0lrlH8GDtKSDSyoFrW7AqnBvdfL9yng8_kbjNUkag-C6sFRCSehAdSWZQBJuRMPDFA2r-1-xtmSsGXqAsdlOjVJ0n3ViYb6n0g_UmT14Cfx-Wp6l8kYZGR6l6yqrGxJb9hx-CD4DZOoUpJV4ksNOusQqpJah0JALMNJMx8tmnMqd9gtZ7hdttVCiYBov8C3Q-lRkEv45K6waw8M1R8c1vOrzd-j1BSb0W1yQeHZxyQS4o7qfl5rYks5gUIrKYcW0C5XkDbgNv6pvnk6yDCXndgghOq7aM-USDkPNeF3xvhbe4cm0L4lxT-Kl-Wra6DyevjDY-y1-CncsxEajpCeeLhrrzY8yYBlek9PWrdNpbYZWnt5-4"
-          }
-        })
-          .then(response => {
-            this.users = response.data
-            console.log(this.users)
-            console.log(this.users[0].firstname)
-            console.log(this.users[0].lastname)
-          })
-          .catch(err => {
-            console.log(err)
-          })
-      },
-
       createUser: function() {
+        console.log('this.users vaut: ')
+        console.log(this.users) // connu ... 
         axios({
           method: 'post',
-          url: 'http://ulysse.idequanet.com/ben/web/api/user/create',
-          data: { user : {
-              firstname : 'Anne',
-              lastname : 'Lagarde', 
-              email : 'anne.lagarde@hetic.net', 
-              password : 'ben'
-          } },
-          headers: {
-            "Access-Control-Allow-Origin": "*"
+          url: 'https://jsonplaceholder.typicode.com/users', 
+          data: {
+            name: 'Helloooo',
+            username: 'Here'
           },
-        }).then(function (response) {
-            console.log(response);
-        }).catch(function (error) {
-            console.log(error);
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        })
+        .then(function (response) {
+          console.log(response.data);
+          console.log('this.users - au moment promesse - vaut: ')
+          console.log(this.users) // non connu ...
+          // this.users = this.users.push(response.data)
+        })
+        .catch(function (error) {
+          console.log(error);
         });
       }
     
--- src/components/Popin.vue
+++ src/components/Popin.vue
@@ -1,12 +1,12 @@
  <template>
  <!-- pop in -->
   <div class="popin">
-    <div class="popin__confirm" v-if="!deletedItem">
+    <div class="popin__confirm" v-if="">
       <p class="popin__text">Are you sure you want to delete this user from the database ?</p>
       <a href="#" class="popin__answer popin__answer-validation" v-on:click="deleteUser(user)">Yes</a>
       <a href="#" class="popin__answer popin__answer-cancelation" v-on:click="closePopin">No</a>
     </div>
-    <div class="popin__validation" v-if="deletedItem">
+    <div class="popin__validation" v-if="">
       <img src="../assets/cross-icon.png" alt="close popin" v-on:click="closePopin">
       <p>User has been correctly deleted ! </p>
     </div> 
@@ -25,7 +25,7 @@
       closePopin: function(event) {
         event.target.parentNode.parentNode.classList.remove('popin-open')
         // update popin 
-        this.deletedItem = false
+        // this.deletedItem = false
 
       },
       deleteConfirmationUser: function(event) {
